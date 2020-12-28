7ce3730b03d81ebecc68efa77bfa34b85d425ebb
SHQ5785 <shq5785@163.com>
2020-12-28 14:36:42 +0800

    my first commit

---
+++ src/views/C7.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <H1>C2 数据</H1>
+    <ul>
+      <li><el-button type="text">C2.1 字符串</el-button></li>
+      <li><el-button type="text">C2.2 数组</el-button></li>
+      <li><el-button type="text">C2.3 集合</el-button></li>
+      <li><el-button type="text">C2.4 数据拷贝</el-button></li>
+      <li><el-button type="text">C2.5 常见问题解析</el-button></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vueCodeDemo'
+}
+</script>
+
+<style scoped>
+ul {
+  text-align: left;
+  margin-top: 20px;
+  margin-left: 10%;
+}
+li{
+  margin-top: 20px;
+}
+</style>
